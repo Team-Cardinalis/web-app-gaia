232a9602851a11793537b6c82b070bc957f7a11f
Team Cardinalis <killianott4@gmail.com>
2025-07-22 15:03:38 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/kit';
 
 const dev = process.argv.includes('dev');
 const base = dev ? '' : '/web-app-gaia'; // Change to your repo name if different
