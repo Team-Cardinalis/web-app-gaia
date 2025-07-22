6b73d2975d54da8e3947a876b52c59c50b5431fd
Team Cardinalis <killianott4@gmail.com>
2025-07-22 16:12:42 +0200

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
 <h1>Bonjour du site GitHub Pages avec Svelte!</h1>
 <p>Ceci est un site simple déployé via GitHub Actions utilisant le framework Svelte.</p>
+<a href="/dashboard" class="text-blue-500 hover:underline">Aller au Tableau de bord</a>
