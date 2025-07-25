14f4e48e71528dbce6890e89297e6fb12a425bed
Team Cardinalis <killianott4@gmail.com>
2025-07-25 04:29:26 +0200

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -22,7 +22,7 @@
 	>
 		<nav class="flex-1">
 			<ul class="flex h-full w-full flex-row items-center gap-x-2 px-2 md:flex-col md:justify-start md:gap-y-2 md:gap-x-0 md:px-0">
-				<NavItem href="/" imgSrc="/favicon.svg" alt={$commonHome} active={$page.url.pathname === '/' } />
+				<NavItem href="/" imgSrc="/icon-black.svg" alt={$commonHome} active={$page.url.pathname === '/' } />
 				<NavItem href="/equipments" imgSrc="/equipment.svg" alt={$commonEquipments} active={$page.url.pathname === '/equipments' } />
 				<NavItem href="/settings" imgSrc="/settings.svg" alt={$commonSettings} active={$page.url.pathname === '/settings' } />
 			</ul>
