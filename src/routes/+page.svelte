7e70a3b11ad98721a1b9ea132fe091a2b4ec2e1c
Team Cardinalis <killianott4@gmail.com>
2025-07-25 03:31:21 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { Flame, Users as HumansIcon, BarChart2, ShoppingCart, Briefcase, Users } from 'lucide-svelte';
+  import { Flame, Users as HumansIcon, Users } from 'lucide-svelte';
   import Card from '$lib/Card.svelte';
   import { t } from '$lib/translations';
   import { get } from 'svelte/store';
@@ -18,9 +18,6 @@
   <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 flex-1 auto-rows-fr grid-flow-dense lg:auto-rows-fr">
     <Card iconSrc={isDay ? '/sun.svg' : '/moon.svg'} color="yellow" title={translate(isDay ? 'dashboard.greeting_day' : 'dashboard.greeting_night')} value="" cardClass="lg:col-span-4 lg:row-span-1" />
     <Card icon={Users} color="blue" title={translate('dashboard.totalUsers')} value="1,234" cardClass="lg:col-span-2 lg:row-span-2" />
-    <Card icon={BarChart2} color="green" title={translate('dashboard.sales')} value="$56,789" cardClass="lg:col-span-1 lg:row-span-1" />
-    <Card icon={ShoppingCart} color="yellow" title={translate('dashboard.newOrders')} value="42" cardClass="lg:col-span-1 lg:row-span-1" />
-    <Card icon={Briefcase} color="purple" title={translate('dashboard.activeProjects')} value="12" cardClass="lg:col-span-2 lg:row-span-1" />
     <Card icon={Flame} color="red" title={translate('dashboard.fireDetected')} value="3" cardClass="lg:col-span-2 lg:row-span-1" />
     <Card icon={HumansIcon} color="orange" title={translate('dashboard.humansDetected')} value="15" cardClass="lg:col-span-2 lg:row-span-1" />
   </div>
